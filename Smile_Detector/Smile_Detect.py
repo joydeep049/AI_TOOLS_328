cec3cd79c49f100a9a53c1b717142b017ab4ae40
Joydeep Tripathy <113792434+crazytrain328@users.noreply.github.com>
2022-12-04 11:17:50 +0530

    Add files via upload

--- Smile_Detector/Smile_Detect.py
+++ Smile_Detector/Smile_Detect.py
@@ -25,17 +25,21 @@ while True:
     for(x,y,w,h) in face_coord:
         cv.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),5)
 
-        face= frame[y:y+h,x:x+w]
+        face= frame[y:y+h, x:x+w]
 
-        grayscaled_face= cv.cvtColor(face, cv.COLOR_BGR2GRAY)
+        blu= cv.GaussianBlur(face, (5,5), 0)
+
+        dilate= cv.dilate(blu, np.ones((3,3)))
+
+        grayscaled_face= cv.cvtColor(dilate, cv.COLOR_BGR2GRAY)
 
         smile_coord= smile_data.detectMultiScale(grayscaled_face,scaleFactor=1.7, minNeighbors=20)
 
-        for (a,b,c,d) in smile_coord:
-            cv.rectangle(face,(a,b),(a+c,b+d),(255,0,255),2)
+        #for (a,b,c,d) in smile_coord:
+          #  cv.rectangle(face,(a,b),(a+c,b+d),(255,0,255),2)
         
-        #if len(smile_coord)> 0:
-         #   cv.putText(frame, 'smiling',(x,y+h+40),fontScale=3,fontFace=cv.FONT_HERSHEY_PLAIN,color=(255,255,255))
+        if len(smile_coord)> 0:
+            cv.putText(frame, 'smiling',(x,y+h+40),fontScale=3,fontFace=cv.FONT_HERSHEY_PLAIN,color=(255,255,255))
     
     cv.imshow('Smile Please',frame)
     key= cv.waitKey(1)
